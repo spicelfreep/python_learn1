23d39702e99c3f8c57446aa05e198632d177abc9
Lin <1543939425@qq.com>
2019-12-06 21:33:46 +0800

    CRLF2LF

--- 网络编程/do_tcp.py
+++ 网络编程/do_tcp.py
@@ -17,11 +17,11 @@
 
 import socket
 
-# 创建一个socket：
+# 创建一个socket：AF_INET指定使用IPv4协议，如果要用更先进的IPv6，就指定为AF_INET6，SOCK_STREAM指定使用面向流的TCP协议
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # 建立连接：
-s.connect( ('www.sina.com.cn', 80) )
+s.connect(('www.sina.com.cn', 80))
 
 # 发送数据：
 s.send(b'GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection: close\r\n\r\n')
--- 进程和线程/多进程/do_subprocess.py
+++ 进程和线程/多进程/do_subprocess.py
@@ -1,1 +1,28 @@
-#!/usr/bin/env python# -*- coding: utf-8 -*-'''@File       :   do_subprocess.py@Time       :   2019/11/29 7:50@Author     :   helin@Version    :   1.0@Contact    :   spicalfreep@163.com@License    :   (C)Copyright 2019-2020@Desc       :   None@Modify Time      @Author     @Version    @Desciption------------      -------     --------    -----------2019/11/29 7:50  helin       1.0         None'''import subprocessprint('$ nslookup www.python.org')r = subprocess.call(['nslookup', 'www.python.org'])print('Exit code:', r)print('$ nslookup')p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)output, err = p.communicate(b'set q=mx\npython.org\nexit\n')print(output.decode('utf-8'))print('Exit code:', p.returncode)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+@File       :   do_subprocess.py
+@Time       :   2019/11/29 7:50
+@Author     :   helin
+@Version    :   1.0
+@Contact    :   spicalfreep@163.com
+@License    :   (C)Copyright 2019-2020
+@Desc       :   None
+
+@Modify Time      @Author     @Version    @Desciption
+------------      -------     --------    -----------
+2019/11/29 7:50  helin       1.0         None
+'''
+
+import subprocess
+
+print('$ nslookup www.python.org')
+r = subprocess.call(['nslookup', 'www.python.org'])
+print('Exit code:', r)
+
+print('$ nslookup')
+p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+print(output.decode('utf-8'))
+print('Exit code:', p.returncode)
