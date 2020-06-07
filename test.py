0199c6e76c1f382e0922a08fae41879c7cf0a6d3
悟佛悟空 <1543939425@qq.com>
2020-06-08 06:56:29 +0800

    词根记忆

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File: test.py
+Author: yourname
+Email: yourname@email.com
+Github: https://github.com/yourname
+Description: 
+"""
+
+def index():
+    """echo some thing."""
+    print("index page")
+
+index()
